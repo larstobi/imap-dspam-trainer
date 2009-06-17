148cca3f16198b6cace66e67eeb441008f975bb3
Lars Tobias Børsting <larstobi@snota.no>
2009-06-17 22:34:19 +0200

    Add support for reading a config file.

--- imap.py
+++ imap.py
@@ -1,9 +1,29 @@
 from imaplib import *
+from string import *
 
-server = IMAP4("")
-server.login("", "")
+configfile = "/home/larstobi/imap/config"
+file = open(configfile, 'r')
+
+while file:
+    line = file.readline()
+    if not line:
+        break
+    var, val = line.split("=")
+    if var == 'host':
+        host = strip(val)
+    elif var == 'port':
+        port = int(strip(val))
+    elif var == 'user':
+        user = strip(val)
+    elif var == 'password':
+        password = strip(val)
+    elif var == 'folder':
+        folder = strip(val)
+
+server = IMAP4(host,port)
+server.login(user,password)
 mboxes = server.list()[1]
-r = server.select("INBOX")
+r = server.select(folder)
 r, data = server.search(None, "(ALL)")
 
 tmpdata = data[0]
